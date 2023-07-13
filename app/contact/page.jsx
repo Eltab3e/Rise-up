fe0acbcef310f1a28339717e5892dc2713288927
Eltab3e <ahmed.eltabiie@gmail.com>
2023-07-13 23:33:03 +0300

    implement contact page ui

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,9 +1,34 @@
-import styles from './page.module.css'
+import Image from 'next/image';
+import styles from './page.module.css';
+import { Button } from '@/components';
 
 const Contact = () => {
   return (
-    <div className={styles.contact}>Contact</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let's Keep in Touch!</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src='/contact.png'
+            alt=''
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type='text' placeholder='Name' className={styles.input} />
+          <input type='text' placeholder='Email' className={styles.input} />
+          <textarea
+            className={styles.textArea}
+            placeholder='Message'
+            cols='30'
+            rows='10'
+          ></textarea>
+          <Button url='#' text='Send' />
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
